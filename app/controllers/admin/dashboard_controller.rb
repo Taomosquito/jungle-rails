46642d4c6b2418a328a2125301bfbf9468fe5028
Taomosquito <S_Peloquin123@live.com>
2024-10-25 23:14:30 -0400

    complete http_basic_authenticate_with integration part 1

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,13 @@
 class Admin::DashboardController < ApplicationController
+  include ActionController::HttpAuthentication::Basic::ControllerMethods
+  before_action :authenticate
+  
   def show
+  end
+
+  private
+
+  def authenticate
+    self.class.http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   end
 end
